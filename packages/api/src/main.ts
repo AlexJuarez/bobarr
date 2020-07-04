fba7f5efb6d38ed585fcdc5530906aa2ae3cb1a2
Alex Juarez <couatli@gmail.com>
2020-07-04 12:56:07 -0700

    changed platform from express to fastify

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,4 +1,8 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { WinstonModule } from 'nest-winston';
 import { UI } from 'bull-board';
 
@@ -7,8 +11,10 @@ import { winstonOptions } from './utils/winston-options';
 
 async function bootstrap() {
   const logger = WinstonModule.createLogger(winstonOptions);
-  const app = await NestFactory.create(AppModule, { logger });
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule,
+    new FastifyAdapter(),
+    { logger });
   app.use('/jobs', UI);
-  await app.listen(4000);
+  await app.listen(4000, '0.0.0.0');
 }
 bootstrap();
